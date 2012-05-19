ee80b0bef72c3ed39fcddd35d484f5e66d7db074
Hadley Magno <hdhzero@gmail.com>
2012-05-19 19:26:39 -0300

    implementado algumas besteiras da classe Frame

--- qpel/src/cpp/Frame.cpp
+++ qpel/src/cpp/Frame.cpp
@@ -4,10 +4,10 @@ Frame::Frame(int w = 640, int h = 480) {
     width  = w;
     height = h;
 
-    pixels = new int*[height];
+    pixels = new Pixel*[height];
 
     for (int i = 0; i < height; ++i) {
-        pixels[i] = new int[width];
+        pixels[i] = new Pixel[width];
     }
 }
 
@@ -19,4 +19,59 @@ Frame::~Frame() {
     delete[] pixels;
 }
 
+void Frame::set_from_yuv420_file(FILE* file) {
+    int i;
+    int j;
+    unsigned char c;
 
+    for (i = 0; i < height; ++i) {
+        for (j = 0; j < width; ++j) {
+            c = (unsigned char) fgetc(file);
+            set_luma(i, j, c);
+        }
+    }
+
+    for (i = 0; i < height; ++i) {
+        for (j = 0; j < width; ++j) {
+            c = (unsigned char) fgetc(file);
+            set_cb(i, j, c);
+            set_cb(i, j + 1, c);
+            set_cb(i + 1, j, c);
+            set_cb(i + 1, j + 1, c);
+        }
+    }
+
+    for (i = 0; i < height; ++i) {
+        for (j = 0; j < width; ++j) {
+            c = (unsigned char) fgetc(file);
+            set_cr(i, j, c);
+            set_cr(i, j + 1, c);
+            set_cr(i + 1, j, c);
+            set_cr(i + 1, j + 1, c);
+        }
+    }
+}
+
+void Frame::set_red(int i, int j, unsigned char red) {
+    pixels[i][j].red = red; 
+}
+
+void Frame::set_green(int i, int j, unsigned char green) {
+    pixels[i][j].green = green;
+}
+
+void Frame::set_blue(int i, int j, unsigned char blue) {
+    pixels[i][j].blue = blue;
+}
+
+void Frame::set_luma(int i, int j, unsigned char luma) {
+    pixels[i][j].red = luma;
+}
+
+void Frame::set_cb(int i, int j, unsigned char cb) {
+    pixels[i][j].green = cb;
+}
+
+void Frame::set_cr(int i, int j, unsigned char cr) {
+    pixels[i][j].blue = cr;
+}
--- qpel/src/cpp/Frame.h
+++ qpel/src/cpp/Frame.h
@@ -1,15 +1,33 @@
 #ifndef FRAME_H
 #define FRAME_H
 
+#include <cstdio>
+
+typedef struct Pixel {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+} Pixel;
+
 class Frame {
     private:
         int width;
         int height;
-        int** pixels;
+        Pixel** pixels;
 
     public:
         Frame(int w = 640, int h = 480);
         ~Frame();
+        void set_from_yuv420_file(FILE* file);
+
+        //pixel functions
+        void set_red(int i, int j, unsigned char red);
+        void set_green(int i, int j, unsigned char green);
+        void set_blue(int i, int j, unsigned char blue);
+
+        void set_luma(int i, int j, unsigned char luma);
+        void set_cb(int i, int j, unsigned char cb);
+        void set_cr(int i, int j, unsigned char cr);
 };
 
 #endif
